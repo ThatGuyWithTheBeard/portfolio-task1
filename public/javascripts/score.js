9ef26e54d0e9e9b2cd072b3b411b0fb032fd3b52
ThatGuyWithTheBeard <nicklas.kristoffersen@hotmail.com>
2020-04-22 14:41:05 +0200

    Move forward and back implemented
    Rooms changed to array
    
    Bound to button
    
    Switch case handling commands

--- public/javascripts/score.js
+++ public/javascripts/score.js
@@ -2,17 +2,27 @@
 
 let highscore;
 let points = 0;
-let currentRoom = 0;
 
-let rooms = {
-
-    0: true,
-    1: false,
-    2: false,
-
-};
-
-let direction = "";
+let rooms = [
+    {
+        number: 0,
+        name: "room 1 AKA start room",
+        description: "there's a painting on the wall",
+        isInRoom: true
+    },
+    {
+        number: 1,
+        name: "room 2 AKA middle room",
+        description: "this rooms has nothing interesting in it",
+        isInRoom: false
+    },
+    {
+        number: 2,
+        name: "room 3 AKA the final room",
+        description: "there's a pit in front of you. You can't go any further",
+        isInRoom: false
+    }
+]
 
 const nonDigit = /\D+/g;
 const digit = /\d+/g;
@@ -30,16 +40,16 @@ $().ready(() => {
     $("#submit-btn").click((event) => {
 
         //event.preventDefault();
-        direction = $("#command-input").val();
+        let command = $("#command-input").val();
         
-        console.log(direction);
+        console.log(rooms);
 
-        console.log();
+        moveRoom(command);
 
     });
     
     
-    console.log($("#highscore").text());
+    /* console.log($("#highscore").text());
     console.log(highscore);
     console.log(isNaN(highscore));
     console.log(typeof highscore);
@@ -48,7 +58,7 @@ $().ready(() => {
     //$("#points").text()
     console.log($("#points").text());
     console.log(typeof $("#points").text());
-    console.log($("#points").text().replace(nonDigit, ""));
+    console.log($("#points").text().replace(nonDigit, "")); */
 });
 
 const addPoint = () => {
@@ -78,60 +88,47 @@ const resetHighscore = () => {
 
 }
 
-const moveRoom = () => {
+
+const moveRoom = (command) => {
     
-}
+    let currentRoom = rooms.filter(room => room.isInRoom === true)[0];
 
-const moveRoom_old = (direction) => {
-
-    const MOVE_FORWARD = "forward";
-    const MOVE_BACK = "back";
-
-    switch(direction) {
-        case MOVE_FORWARD:
-
-            if(currentRoom + 1 < rooms[rooms.length - 1]) {
-                currentRoom += 1;
-                $("#status").text(`Moved succesfully! You're now in room ${currentRoom}.`);
-            } else {
-
-                $("#status").text(`You can't move that way. You're in room ${currentRoom}.`);
-
-            }
+    const FORWARD = "forward";
+    const BACK = "back";
+    
+    switch(command) {
+        case FORWARD:
             
-            console.log("MOVE_FORWARD activated.");
+            if(currentRoom.number < rooms.length - 1) {
+                
+                rooms[currentRoom.number].isInRoom = false;
+                rooms[currentRoom.number + 1].isInRoom = true;
+                console.log(`You move forward into ${rooms[currentRoom.number + 1].name}`);
 
-        break;
-
-        case MOVE_BACK:
-
-            if(rooms[0] < currentRoom) {
-                currentRoom -= 1;
-                $("#status").text(`Moved succesfully! You're now in room ${currentRoom}.`);
-            } else {
-
-                $("#status").text(`You can't move that way. You're in room ${currentRoom}.`);
-
-            }
+            } else
+                console.log(`You can't move forward here`);
             
-            console.log("MOVE_BACK activated.");
+            break;
 
-        break;
+        case BACK:
+
+            if(currentRoom.number > 0) {
+
+                rooms[currentRoom.number].isInRoom = false;
+                rooms[currentRoom.number - 1].isInRoom = true;
+                console.log(`You move back into ${rooms[currentRoom.number - 1].name}`);
+                
+            } else
+                console.log(`You can't move back any further`);
+
+            break;
 
         default:
 
-            console.log("Default triggered.")
-            
-        break;
+            console.log(`The command "${command}" is not recognized`)
+        
+            break;
 
     }
-
-}
-
-const moveForward = () => {
-
-}
-
-const moveBackward = () => {
 
 }
