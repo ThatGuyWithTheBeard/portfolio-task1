a70d702f853e0ded0ccbada7eabe66c2b7526b08
ThatGuyWithTheBeard <nicklas.kristoffersen@hotmail.com>
2020-05-13 12:16:54 +0200

    Stripped

--- models/item.js
+++ models/item.js
@@ -1,28 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// Create user Schema & model
+// Create item Schema & model
 const ItemSchema = new Schema({
     name: {
         type: String,
         required: true
     },
-    type: {
+    /* type: {
         type: String,
         default: "Unknown"
-    },
+    }, */
     isPickedUp: {
         type: Boolean,
         default: false
     },
-    pickedUpBy: {
+    /* pickedUpBy: {
         type: String,
         default: "No one"
     },
     room: {
         type: Number,
-        required: [true, "Item needs to be in a room."]
-    }
+        //required: [true, "Item needs to be in a room."]
+    } */
 });
 
 const User = mongoose.model("item", ItemSchema);
